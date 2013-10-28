075f861ecc25fa622eb769cf50006da9d3a0bc2f
guang.long <gwong.long.2013@gmail.com>
2013-10-27 22:09:53 -0400

    logic

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,11 +1,13 @@
-<form method='POST' action='/users/p_signup'>
+<form method='POST'  action='/users/p_signup'>
 
     First Name<br>
     <input type='text' name='first_name'>
+     <div class="error" id="firstNameValidation"> </div>
     <br><br>
 
     Last Name<br>
     <input type='text' name='last_name'>
+    <div class="error" id="lastNameValidation"> </div>
     <br><br>
 
     Email<br>
@@ -15,10 +17,11 @@
 
     Password<br>
     <input type='password' name='password' onKeyUp="javascript:displayStrength();">
-    
+     <div class="error" id="passwordValidation"> </div>
     <br><br>
     Password Strength:
     <div id='pwStrength' class='pwStrength'>  </div> <br>
      <br><br>
-    <input type='submit' value='Sign up'>
+    <!--input type='submit' onclick="javascript:onSubmit();"value='Sign??up'-->
+    <button onclick="return onSubmit();">Sign Up</button>
 </form>
